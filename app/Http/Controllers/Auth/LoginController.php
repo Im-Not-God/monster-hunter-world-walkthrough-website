85cdcc9b2727c71f0d883af833e306cf07c1ff64
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2023-04-06 12:28:13 +0800

    add admin login authorize

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,40 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+        //$this->adminAuthorize();
+    }
+
+
+
+    // public function superUserAuthorize()
+    // {
+    //     if (Gate::allows('isSuperUser')) {
+    //         return view('auth.login');
+    //     }
+    // }
+    // public function adminAuthorize()
+    // {
+    //     if (Gate::denies('checkAdminMacAddress')) {
+    //         return view('auth.login', ['url' => 'admin']);
+    //     }else
+    //         return view('auth.login');
+
+    // }
+
+
+    public function adminLogin(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+        if (Auth::guard('admin')->attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ], $request->get('remember'))) {
+            return redirect()->intended('/admin');
+        }
+        return back()->withInput($request->only('email', 'remember'));
     }
 }
