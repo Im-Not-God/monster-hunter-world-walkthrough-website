11eec04b1f9fa7e2f2bc0b4fcbeb354f6a7baec1
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2023-04-14 02:13:51 +0800

    done weapon controller

--- app/Http/Controllers/WeaponController.php
+++ app/Http/Controllers/WeaponController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 class WeaponController extends Controller
 {
     //
+    public $rarity = [];
+
     public function getWeapon($type)
     {
         // Set the API endpoint URL
@@ -26,69 +28,127 @@ class WeaponController extends Controller
         } else {
             // Process the data
             // ..
-            return view('example', ['weaponTree'=>$this->generateWeaponTree($data), 'total'=>count($data), 'weapons'=>$data]);
+            return view('example', ['weaponTree' => $this->generateWeaponTree($data, $type), 'type' => $type]);
         }
     }
 
-    public function generateWeaponTree($data, $parentId = null, $level=0, $token = false)
-    { 
-        // $output = '<ul>';
-
-        // foreach($data as $weapon){
-        //     if($weapon['crafting']['previous']==$parentId){
-        //         $output .= '<li>'.$weapon['name'];
-        //         if(count($weapon['crafting']['branches']) != 0){
-        //             $output .= $this->generateWeaponTree($data, $weapon['id']);
-        //         }else{
-        //             $output .= '</li>';
-        //         }
-        //     }
-        // }
-        // return $output .= '</ul>';
-
-        // $output = '<div class="dul tree">';
-
-        // foreach($data as $weapon){
-        //     if($weapon['crafting']['previous']==$parentId){
-        //         $output .= '<div class="dli tree">'.$weapon['name'];
-        //         if(count($weapon['crafting']['branches']) != 0){
-        //             $output .= $this->generateWeaponTree($data, $weapon['id']);
-        //         }else{
-        //             $output .= '</div>';
-        //         }
-        //     }
-        // }
-        // return $output .= '</div>';
-
+    public function generateWeaponTree($data, $type, $parentId = null, $level = 0, $token = false)
+    {
         $output = '';
 
-        foreach($data as $weapon){
-            if($weapon['crafting']['previous']==$parentId){
-                // if(strlen($tab)){
-                //     $output .= '<tr><td>'.$tab."┗".$weapon['name'].'</td></tr>';
-                // }
-                // if($parentId==null){
-                //     $tab="";
-                //     // $level=1;
-                // }
-                if($token)
+        foreach ($data as $weapon) {
+            if ($weapon['crafting']['previous'] == $parentId) {
+                if ($token)
                     $level--;
-                
-                $tab="";
-                for($i=0; $i< abs($level); $i++){
-                    $tab.="&emsp;";
-                }
-                $output .= '<tr><td class="weaponRow">'.$tab.$weapon['id']." ".$weapon['name']." pid:$parentId".'</td></tr>';
 
-                if(count($weapon['crafting']['branches']) != 0){
+                $tab = "";
+                for ($i = 0; $i < abs($level); $i++) {
+                    $tab .= "&emsp;";
+                }
+                $output .= '<tr data-id="' . $weapon['id'] . '">';
+                $output .= '<td class="weaponRow" data-pid="' . $parentId . '">' . $tab . $weapon['name'] . '</td>';
+                $output .= '<td class="text-center">' . $weapon['rarity'] . '</td>';
+                $output .= '<td class="text-center">' . $weapon['attack']['display'] . '</td>';
+
+                if ($type != "light-bowgun" && $type != "heavy-bowgun") {
+                    if (array_key_exists('elements', $weapon) && $weapon['elements']) {
+                        if ($weapon['elements'][0]["hidden"]) {
+                            $output .= '<td class="text-center text-secondary"><img src="/img/elements/' . $weapon['elements'][0]['type'] . '.png" alt="' . $weapon['elements'][0]['type'] . '" width="25px">' . ': (' . $weapon['elements'][0]['damage'] . ')</td>';
+                        } else {
+                            $output .= '<td class="text-center"><img src="/img/elements/' . $weapon['elements'][0]['type'] . '.png" alt="' . $weapon['elements'][0]['type'] . '" width="25px">' . ': ' . $weapon['elements'][0]['damage'] . '</td>';
+                        }
+                    } else {
+                        $output .= '<td class="text-center"> - </td>';
+                    }
+                }
+
+                if (array_key_exists('durability', $weapon)) {
+                    $output .= '<td class="align-center"><div class="progress" style="max-width: 100%; min-width: 100px; border-radius: unset;border:1px solid black;">';
+                    $output .= '
+                    <div class="progress-bar bg-danger" style="width: ' . ($weapon['durability'][0]['red'] / 4) . '%;">
+                        &nbsp; 
+                    </div> 
+                    <div class="progress-bar" style="background-color:orange; width: ' . ($weapon['durability'][0]['orange'] / 4) . '%;">
+                    &nbsp; 
+                        &nbsp; 
+                    </div> 
+                    <div class="progress-bar bg-warning" style="width: ' . ($weapon['durability'][0]['yellow'] / 4) . '%;">
+                    &nbsp; 
+                        &nbsp; 
+                    </div> 
+                    <div class="progress-bar bg-success" style=" width: ' . ($weapon['durability'][0]['green'] / 4) . '%;">
+                    &nbsp; 
+                        &nbsp; 
+                    </div> 
+                    <div class="progress-bar" style="width: ' . ($weapon['durability'][0]['blue'] / 4) . '%;">
+                    &nbsp; 
+                        &nbsp; 
+                    </div> 
+                    <div class="progress-bar bg-white" style="width: ' . ($weapon['durability'][0]['white'] / 4) . '%;">
+                    &nbsp; 
+                        &nbsp; 
+                    </div>';
+                    $output .= '</div></td>';
+                }
+
+                if ($type == "heavy-bowgun") {
+                    if (array_key_exists('specialAmmo', $weapon)) {
+                        if ($weapon['specialAmmo'])
+                            $output .= '<td class="text-center">' . $weapon['specialAmmo'] . '</td>';
+                        else
+                            $output .= '<td class="text-center"> - </td>';
+                    }
+                }
+
+                if (array_key_exists('coatings', $weapon)) {
+                    $output .= '<td class="text-center">';
+                    $lastIndex = count($weapon['coatings']) -1;
+                    foreach ($weapon['coatings'] as $index=>$coating) {
+                        $output .= '<img src="/img/coatings/' . $coating . '.png" alt="' . $coating . '" width="25px">' . $coating;
+                        if($index < $lastIndex)
+                        $output .= ',';
+                    }
+                    $output .= '</td>';
+                }
+
+                if (array_key_exists('phial', $weapon)) {
+                    if ($weapon['phial'])
+                        $output .= '<td class="text-center">' . $weapon['phial']['type'] . ($weapon['phial']['damage'] ? (': ' . $weapon['phial']['damage']) : '') . '</td>';
+                    else
+                        $output .= '<td class="text-center"> - </td>';
+                }
+
+                if ($weapon['slots']) {
+                    $output .= '<td class="text-center">';
+                    foreach ($weapon['slots'] as $slot) {
+                        $output .= '<img src="/img/slots/' . $slot['rank'] . '.png" alt="' . $slot['rank'] . '" width="25px">';
+                    }
+                    $output .= '</td>';
+                } else {
+                    $output .= '<td class="text-center"> - </td>';
+                }
+
+                if ($weapon['attributes']) {
+                    $output .= '<td class="text-center">';
+                    foreach ($weapon['attributes'] as $name => $value) {
+                        if ($name != "damageType" && $value)
+                            $output .= $name . ": " . $value . '</br>';
+                    }
+                    $output .= '</td>';
+                } else {
+                    $output .= '<td class="text-center"> - </td>';
+                }
+
+                $output .= '</tr>';
+                if (count($weapon['crafting']['branches']) != 0) {
                     $token = true;
-                    $output .= $this->generateWeaponTree($data, $weapon['id'], $level++, $token);
-                }else{
+                    $output .= $this->generateWeaponTree($data, $type, $weapon['id'], $level++, $token);
+                } else {
                     $token = false;
                 }
             }
         }
-        
-        return $output.="";
+
+        return $output .= "";
     }
 }
