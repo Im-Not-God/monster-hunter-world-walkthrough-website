ebb925d46cff8708d6a9db9eb3f76118de0baec4
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2023-04-14 05:25:34 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,54 @@
-@extends('layouts.app')
 
+<title>Homepage</title>
+
+@extends('layouts.navigation')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<style>
+    body {
+        background-image: url('{{ asset("/img/monsterhunterbackground.jpg") }}');
+        background-color: rgba(46, 50, 54, 0.7);
+        background-blend-mode: multiply;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+        font-family: "Marcellus SC";
+    }
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    .content {
+        position: relative;
+        bottom: 50px;
+        left: 50%;
+        transform: translateX(-50%);
+        text-align: center;
+        padding-left: 130px;
+        padding-right: 130px;
+        padding-top: 80px;
+        font-size: 1.3rem;
+        color: white;
+        border-radius: 10px;
+    }
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    .image-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    .image {
+        max-width: 400px;
+        max-height: 400px;
+        display: block;
+        margin: 0 auto;
+    }
+</style>
+<div class="image-container">
+    <img class="image" src="{{ asset('/img/homepageimg.jpg') }}" alt="My Image">
+</div>
+
+<div class="content">
+    <h3>About The Game<h3>
+            <p>Welcome to a new world! In Monster Hunter: World, the latest installment in the series, you can enjoy the ultimate hunting experience, using everything at your disposal to hunt monsters in a new world teeming with surprises and excitement.</p>
 </div>
 @endsection
