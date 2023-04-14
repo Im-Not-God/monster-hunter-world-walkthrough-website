242ac7b4f26aaffb5a5926b5fe9bce9e3d688a67
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2023-04-15 05:32:49 +0800

    Update authorize.blade.php

--- resources/views/authorize.blade.php
+++ resources/views/authorize.blade.php
@@ -9,25 +9,13 @@
   <title>Authorize</title>
 
   <style>
-    /* th {
-      border-left: 1px solid black;
-      border-right: 1px solid black;
-    }
-
-    td {
-      border-left: 1px solid black;
-      border-right: 1px solid black;
-    } */
     thead {
       border-bottom: 1px solid white;
     }
 
-    tbody tr:hover {
-      cursor: pointer;
-    }
-
     th,
-    td {
+    td,
+    h3 {
       text-align: center;
     }
   </style>
@@ -37,6 +25,7 @@
 <body>
   <div class="container mt-4">
 
+    <h3>User List</h3>
     <table class="table table-dark table-hover">
       <thead>
         <tr>
@@ -54,13 +43,17 @@
           <td>{{$user->email}}</td>
           <td>{{$user->role}}</td>
           <td>
-          <form id="logout-form" action="" method="POST">
-            @csrf
-            @if($user->role=='user')
-            <button type="submit" class="btn btn-primary">Be author</button>
-            @else
-            <button type="submit" class="btn btn-secondary">Be user</button>
-            @endif
+            <form action="" method="POST">
+              <input type="hidden" name="id" value="{{$user->id}}" readonly>
+              @csrf
+              @if($user->role=='user')
+              <button type="submit" name="role" value="author" class="btn btn-primary">Be author</button>
+              @else
+              <button type="submit" name="role" value="user" class="btn btn-secondary">Be user</button>
+              @endif
+              @if($isSuperUser && !$user->admin)
+              <button type="submit" name="admin" value="add" class="btn btn-warning">Be admin</button>
+              @endif
             </form>
           </td>
         </tr>
@@ -68,9 +61,40 @@
       </tbody>
     </table>
 
+    <h3>Admin List</h3>
+    <table class="table table-dark table-hover">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          @if($isSuperUser)
+          <th>Action</th>
+          @endif
+        </tr>
+      </thead>
+      <tbody>
+
+        @foreach ($admins as $admin)
+        <tr class='clickable-row'>
+          <td>{{$admin->name}}</td>
+          <td>{{$admin->email}}</td>
+          @if($isSuperUser)
+          <td>
+            <form action="" method="POST">
+              <input type="hidden" name="id" value="{{$admin->id}}" readonly>
+              @csrf
+              <button type="submit" name="admin" value="remove" class="btn btn-danger">Remove</button>
+            </form>
+          </td>
+          @endif
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
   </div>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> -->
   <script>
     // $(document).ready(function($) {
     //   $(".clickable-row").click(function() {
