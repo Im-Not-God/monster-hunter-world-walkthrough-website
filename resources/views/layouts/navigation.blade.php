5652e9c9af252fa7f11c030212590b9639f15df4
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2023-04-14 22:11:52 +0800

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,8 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    
+
     <link href="{{ asset('/css/css.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
 </head>
@@ -26,17 +30,41 @@
                         <a class="nav-link" href="{{ url('/') }}">Home</a>
                     </li>
                     <li class="nav-item {{ Request::is('directory') ? 'active' : '' }}">
-                        <a class="nav-link" href="/directory/weapon_list">Directory</a>
+                        <a class="nav-link" href="{{ url('/directory') }}">Directory</a>
                     </li>
                     <li class="nav-item {{ Request::is('posts') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ url('/posts') }}">Posts</a>
                     </li>
+                    
+                </ul>
+
+                <ul class="navbar-nav ms-auto" style="height:0px; margin-right: 270px;">
                     @if(Auth::guard()->check() || Auth::guard('admin')->check())
-                    <li class="nav-item {{ Request::is('profile') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ url('/profile') }}">Profile</a>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            @if(Auth::guard('admin')->check())
+                            {{ Auth::guard('admin')->user()->name }}
+                            @else
+                            {{ Auth::user()->name}}
+                            @endif
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
                     </li>
                     @endif
-
                 </ul>
 
                 @if(!(Auth::guard()->check() || Auth::guard('admin')->check()))
