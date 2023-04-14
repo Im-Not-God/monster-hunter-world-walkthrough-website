b583cb2ac36c3fa225a84c62a443f53a1e0a7a1e
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2023-04-15 03:33:40 +0800

    add small

--- resources/views/monster_list.blade.php
+++ resources/views/monster_list.blade.php
@@ -14,46 +14,82 @@
 
     @extends('layouts.navigation')
     @section('content')
-        <div class="d-flex">
-            <div>
-                @include('sideNav')
-            </div>
-            <div class="container">
-                <?php
-                // Set the API endpoint URL
-                $url = 'https://mhw-db.com/monsters?q={"type":"large"}';
-            
-                // Fetch the JSON data from the API endpoint
-                $json_data = file_get_contents($url);
-            
-                // Decode the JSON data into a PHP array
-                $data = json_decode($json_data, true);
-            
-                // Check if the JSON data was successfully decoded
-                if ($data === null) {
-                    // Handle the decoding error
-                    echo "Error decoding JSON data";
-                } else {
-                    // Process the data
-                    // ..
-                ?>
-                    <div class="row">
-                        @foreach($data as $result)
-                        <div class="col-sm-2 text-center">
-                            <a href="#?id={{$result['id']}}" class="text-decoration-none">
-                                <img src="/img/monster icon/{{$result['name']}}.png" width="100px" alt=""><br>
-                                {{$result['name']}}
-                            </a>
-                        </div>
-                        @endforeach
-                    </div>
-                <?php
-            
-                }
-            
-                ?>
-            </div>
+    <div class="d-flex">
+        <div>
+            @include('sideNav')
         </div>
+        <div class="container">
+            <h2>Large Monsters</h2>
+            <?php
+            // Set the API endpoint URL
+            $url = 'https://mhw-db.com/monsters?q={"type":"large"}';
+
+            // Fetch the JSON data from the API endpoint
+            $json_data = file_get_contents($url);
+
+            // Decode the JSON data into a PHP array
+            $data = json_decode($json_data, true);
+
+            // Check if the JSON data was successfully decoded
+            if ($data === null) {
+                // Handle the decoding error
+                echo "Error decoding JSON data";
+            } else {
+                // Process the data
+                // ..
+            ?>
+                <div class="row">
+                    @foreach($data as $result)
+                    <div class="col-sm-2 text-center">
+                        <a href="#?id={{$result['id']}}" class="text-decoration-none">
+                            <img src="/img/monster icon/{{$result['name']}}.png" width="100px" alt=""><br>
+                            {{$result['name']}}
+                        </a>
+                    </div>
+                    @endforeach
+                </div>
+            <?php
+
+            }
+
+            ?>
+            </br>
+            <h2>Small Monsters</h2>
+            <?php
+            // Set the API endpoint URL
+            $url = 'https://mhw-db.com/monsters?q={"type":"small"}';
+
+            // Fetch the JSON data from the API endpoint
+            $json_data = file_get_contents($url);
+
+            // Decode the JSON data into a PHP array
+            $data = json_decode($json_data, true);
+
+            // Check if the JSON data was successfully decoded
+            if ($data === null) {
+                // Handle the decoding error
+                echo "Error decoding JSON data";
+            } else {
+                // Process the data
+                // ..
+            ?>
+                <div class="row">
+                    @foreach($data as $result)
+                    <div class="col-sm-2 text-center">
+                        <a href="#?id={{$result['id']}}" class="text-decoration-none">
+                            <img src="/img/monster icon/{{$result['name']}}.png" width="100px" alt=""><br>
+                            {{$result['name']}}
+                        </a>
+                    </div>
+                    @endforeach
+                </div>
+            <?php
+
+            }
+
+            ?>
+        </div>
+    </div>
     @endsection
 </body>
 
