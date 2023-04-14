c67059d07925d08c2c0e9e361358556fa1d50a24
GohJunLe <gohjunle100@gmail.com>
2023-04-14 13:46:39 +0800

    add getWeaponDetails

--- app/Http/Controllers/WeaponController.php
+++ app/Http/Controllers/WeaponController.php
@@ -151,4 +151,24 @@ class WeaponController extends Controller
 
         return $output .= "";
     }
+
+    public function getWeaponDetails($id){
+        $url = 'https://mhw-db.com/weapons/'.$id;
+
+        // Fetch the JSON data from the API endpoint
+        $json_data = file_get_contents($url);
+
+        // Decode the JSON data into a PHP array
+        $data = json_decode($json_data, true);
+
+        // Check if the JSON data was successfully decoded
+        if ($data === null) {
+            // Handle the decoding error
+            echo "Error decoding JSON data";
+        } else {
+            // Process the data
+            // ..
+            return view('insideWeapon', ['result' => $data]);
+        }
+    }
 }
