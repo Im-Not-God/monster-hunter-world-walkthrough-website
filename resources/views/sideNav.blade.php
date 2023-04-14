8858f30877809a07dd61ae296ccaac1f1bd780a7
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2023-04-14 17:40:01 +0800

    Add files via upload

---
+++ resources/views/sideNav.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>Homepage</title>
+    <link href="{{ asset('/css/css.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="sidenav">
+        <a href="weapon">weapon</a>
+        <a href="monster">monster</a>
+        <a href="#">Link 3</a>
+        <a href="#">Link 4</a>
+    </nav>
+</body>
+
+</html>
