16b8f757b34a1a23b6c816cb03c826c989a2f4de
Im-Not-God <yaojun12345678910@gmail.com>
2023-04-16 22:03:07 +0800

    update kernal, postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
@@ -13,11 +14,10 @@ class PostController extends Controller
     public function getAllPosts()
     {
         $posts = Post::with('user')->where('state', 1)->get();
-        //return $posts;
         return view('post', ['posts' => $posts]);
     }
 
-    public function showPost($id)
+    public function showPostAndCommends($id)
     {
         $post = Post::with('user')->where('id', $id)->get();
         return view('detailPost', ['post' => $post[0]]);
@@ -32,16 +32,15 @@ class PostController extends Controller
                 'title' => $req['title'],
                 'content' => $req['content'],
             ]);
-
-            return redirect('/posts');
         }
+
+        return redirect('/posts');
     }
 
     public function edit($id)
     {
         if (Gate::allows('isAuthor')) {
             return view('update', ['post' => Post::find($id)]);
-            
         }
     }
 
@@ -52,8 +51,9 @@ class PostController extends Controller
             $post->title = $req['title'];
             $post->content = $req['content'];
             $post->save();
-            return redirect('/posts/'.($req->id));
         }
+
+        return redirect('/posts/'.($req->id));
     }
 
     public function delete(Request $req)
@@ -63,6 +63,25 @@ class PostController extends Controller
         } else if (Gate::allows('isAdmin')) {
             Post::where('id', $req->id)->update(['state' => 0]);
         }
-        return redirect("/home");
+        return redirect("/posts");
     }
+
+    public function comment(Request $req)
+    {
+        if(Auth::guard('admin')->check() || Auth::guard('superuser')->check()){
+            Comment::create([
+                'user_id' => Auth::guard('admin')->user()->user_id,
+                'content' => $req->comment,
+                'post_id' => $req->postId
+            ]);
+        }else
+        Comment::create([
+            'user_id' => Auth::user()->id,
+            'content' => $req->comment,
+            'post_id' => $req->postId
+        ]);
+
+        return redirect('/posts/'.($req->postId));
+    }
+    
 }
